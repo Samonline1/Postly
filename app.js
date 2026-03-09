54d3f75029a97660856fae13765055fc4f7c4fd9
Sam Online <mrsampvt@gmail.com>
2026-03-09 12:25:29 +0530

    Auth

--- app.js
+++ app.js
@@ -1,8 +1,78 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const bcrypt = require("bcrypt");
+const JWT = require('jsonwebtoken');
+
+const cookieParser = require("cookie-parser");
+const userModel = require("./modals/user");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send("Oh hi world!")
-})
+    res.render("signup")
+});
+
+
+app.post('/create', async(req, res) => {
+
+
+    const { username, email, password, age } = req.body ;
+
+    console.log(req.body);
+    
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            console.log(hash);
+            
+            let user = await userModel.create({
+                username,
+                email,
+                password: hash,
+                age,
+            });
+
+            let token = JWT.sign({ email }, "shhhh");
+            res.cookie("token", token);
+            console.log(user);
+            res.redirect('/')
+        });
+    });
+    
+});
+
+app.get('/login', (req, res) => {
+    res.render("login")
+});
+
+app.post('/login', async(req, res) => {
+
+    const email = req.body.email ;
+
+    const user = await userModel.findOne({email: email});
+    if (!user) return res.send("something went wrong!");
+
+        bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+            let token = JWT.sign({ email : user.email }, "shhhh");
+            res.cookie("token", token)
+            res.send("yes you can login!")
+        } else {
+            res.send("bhak bsdk!")
+        }
+    })
+
+});
+
+app.get('/logout', (req, res) => {
+    res.cookie("token", "")
+    res.redirect("/");
+});
 
 app.listen(3000);
+
+
---
+++ modals/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/authtest`);
+
+const userSchema = mongoose.Schema({
+    username: String, 
+    email : String,
+    password: String, 
+    age: Number,
+    posts: Array
+})
+
+module.exports = mongoose.model("user", userSchema);
